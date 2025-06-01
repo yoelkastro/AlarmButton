bcf116745cf46b24f97e77ff5184393bfa78ffbf
yoelkastro <yoelkastro@gmail.com>
2025-06-01 19:33:03 +0100

    Implement button press logic with interrupts
    Previous polling implementation resulted in inability to read button press after some time had passed

--- button/src/AlarmButton/AlarmButton.ino
+++ button/src/AlarmButton/AlarmButton.ino
@@ -6,13 +6,24 @@
 const int buttonPin = 32;
 const int ledPin =  12;
 
+AsyncWebServerRequest *curRequest = NULL;
+
 AsyncWebServer server(80);
+
+void IRAM_ATTR handleInterrupt() {
+  if(curRequest != NULL){
+    digitalWrite(ledPin, LOW);
+    curRequest->send(200, "text/plain", "OK");
+    curRequest = NULL;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), handleInterrupt, RISING);
 
   WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
@@ -26,13 +37,9 @@ void setup() {
 
   server.on("/startAlarm", HTTP_GET, [] (AsyncWebServerRequest *request) {
     digitalWrite(ledPin, HIGH);
-    
-    while(!digitalRead(buttonPin));
-    
-    digitalWrite(ledPin, LOW);
-
-    request->send(200, "text/plain", "OK");
+    curRequest = request;
   });
+
   // TO-DO: Implement async alarm stopped for response not received
 
   server.begin();
